ffb071573e0db3b06a05cffe33aa1ef262c55b47
Mojahidul Islam <bdemon245@gmail.com>
2023-01-20 16:59:56 +0600

    added another import in component/index

--- resources/js/Components/index.js
+++ resources/js/Components/index.js
@@ -1,4 +1,5 @@
 import NavBar from './NavBar/NavBar';
 import Hero from './Hero/Hero';
 import Section from './Landing/Section'
-export { NavBar, Hero, Section }
+import SearchBox from './SearchBox/SearchBox'
+export { NavBar, Hero, Section, SearchBox }
