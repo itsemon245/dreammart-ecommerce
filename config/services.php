bbeb12e859c44fb785469c23dd6b88ae4e1e4fdb
Mojahidul Islam <bdemon245@gmail.com>
2023-01-28 20:11:01 +0600

    added a new field in user table and changed the redirect url of google

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => '/oauth',
+        'redirect' => 'http://localhost:8000/auth/google/callback',
     ],
 
 ];
