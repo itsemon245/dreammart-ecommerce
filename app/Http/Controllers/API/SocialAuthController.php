a16f5b4641ae61279b88b0acd0d5640eb592ed07
Mojahidul Islam <bdemon245@gmail.com>
2023-01-29 10:44:12 +0600

    added logic to handle callback and redirects from google oauth

--- app/Http/Controllers/API/SocialAuthController.php
+++ app/Http/Controllers/API/SocialAuthController.php
@@ -2,10 +2,39 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialAuthController extends Controller
 {
-    //
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+
+    public function handleCallback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+        } catch (\Exception $e) {
+            return redirect('/login');
+        }
+
+        $user = User::updateOrCreate([
+            'google_id' => $googleUser->id,
+        ], [
+            'name' => $googleUser->name,
+            'email' => $googleUser->email,
+            'google_token' => $googleUser->token,
+            'google_refresh_token' => $googleUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/')->with('success', "You are logged in");
+    }
 }
