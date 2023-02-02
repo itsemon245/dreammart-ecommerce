dee4a38bdf59b552c1d951ace6e0921018067880
Mojahidul Islam <bdemon245@gmail.com>
2023-02-02 22:18:31 +0600

    added avater select option

--- resources/views/components/backend/form/select-avater.blade.php
+++ resources/views/components/backend/form/select-avater.blade.php
@@ -1,9 +1,10 @@
-<div class="mb-3">
-    <label for="defaultSelect" class="form-label">Select Avater</label>
-    <select id="defaultSelect" class="form-select">
-      <option>Default select</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </select>
+<div class="mb-3 d-flex gap-2">
+    <label class="d-flex flex-column gap-2 justify-content-center">
+            <img src="https://api.dicebear.com/5.x/adventurer/svg?seed=Simba&scale=90&radius=50&backgroundType=gradientLinear&features=birthmark,blush,freckles&featuresProbability=25&glassesProbability=20&backgroundColor=ffd5dc,b6e3f4&size=64" alt="">
+            <input type="radio" class="" value="https://api.dicebear.com/5.x/adventurer/svg?seed=Simba&scale=90&radius=50&backgroundType=gradientLinear&features=birthmark,blush,freckles&featuresProbability=25&glassesProbability=20&backgroundColor=ffd5dc,b6e3f4" name="avater">
+    </label>
+    <label class="d-flex flex-column gap-2 justify-content-center">
+            <img src="https://api.dicebear.com/5.x/adventurer/svg?seed=Sammy&scale=90&radius=50&backgroundType=gradientLinear&features=birthmark,blush,freckles&featuresProbability=25&glassesProbability=20&backgroundColor=ffd5dc,b6e3f4&size=64" alt="">
+            <input type="radio" class="" value="https://api.dicebear.com/5.x/adventurer/svg?seed=Sammy&scale=90&radius=50&backgroundType=gradientLinear&features=birthmark,blush,freckles&featuresProbability=25&glassesProbability=20&backgroundColor=ffd5dc,b6e3f4" name="avater">
+    </label>
 </div>
