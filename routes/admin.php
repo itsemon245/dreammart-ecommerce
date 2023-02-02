8a4069e24bdbff3bad19af9f6bf65f7937dc9cba
Mojahidul Islam <bdemon245@gmail.com>
2023-02-02 17:50:41 +0600

    refactored admin routes with controller grouping

--- routes/admin.php
+++ routes/admin.php
@@ -3,18 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\DashboardController;
-
-
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::prefix('admin')->group(function () {
     Route::name('product.')->group(function () {
         Route::get('add-product', [ProductController::class, 'addProduct'])->name('add');
         Route::get('view-product', [ProductController::class, 'viewProduct'])->name('view');
     });
-    Route::name('admin.')->group(function () {
-        Route::get('/', [DashboardController::class, 'index'])->name("dashboard");
-        Route::get('login', [DashboardController::class, 'redirectToLogin'])->name("login");
-        Route::get('profile', [DashboardController::class, 'profile'])->name('profile');
-        Route::get('register', [DashboardController::class, 'redirectToRegister'])->name("register");
+    Route::name('admin.')->controller(DashboardController::class)->group(function () {
+        Route::get('/', 'index')->name("dashboard");
+        Route::get('login', 'redirectToLogin')->name("login");
+        Route::get('profile', 'profile')->name('profile');
+        Route::get('register', 'redirectToRegister')->name("register");
     });
 });
