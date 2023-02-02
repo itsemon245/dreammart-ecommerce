9b71351d87b83464379b52ff21b7fe8b6d30410a
Mojahidul Islam <bdemon245@gmail.com>
2023-02-02 17:48:34 +0600

    refactored admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -5,14 +5,16 @@ use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\DashboardController;
 
 
-Route::get('/admin', [DashboardController::class, 'index'])->name("admin.dashboard");
-Route::get('/admin-login', [DashboardController::class, 'redirectToLogin'])->name("admin.login");
-Route::get('/admin-register', [DashboardController::class, 'redirectToRegister'])->name("admin.register");
 
 Route::prefix('admin')->group(function () {
     Route::name('product.')->group(function () {
         Route::get('add-product', [ProductController::class, 'addProduct'])->name('add');
         Route::get('view-product', [ProductController::class, 'viewProduct'])->name('view');
     });
-    Route::name('admin.')->get('profile', [DashboardController::class, 'profile'])->name('profile');
+    Route::name('admin.')->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name("dashboard");
+        Route::get('login', [DashboardController::class, 'redirectToLogin'])->name("login");
+        Route::get('profile', [DashboardController::class, 'profile'])->name('profile');
+        Route::get('register', [DashboardController::class, 'redirectToRegister'])->name("register");
+    });
 });
