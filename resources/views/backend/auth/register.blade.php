20c5e329e7fd802c07b49d1ecac1a8e1a606da02
Ariful Islam <arif98741@gmail.com>
2023-02-04 15:09:01 +0600

    fix: image path issue for storage link and validation rule for email solved

--- resources/views/backend/auth/register.blade.php
+++ resources/views/backend/auth/register.blade.php
@@ -10,8 +10,9 @@
 >
     <x-backend.head />
     <body class="d-flex justify-content-center p-5">
-            
+
         <div class="card mb-4" style="width: 50%">
+
             <div class="card-header d-flex justify-content-between align-items-center">
             <h5 class="mb-0">Log in</h5>
             </div>
@@ -20,7 +21,8 @@
                 @csrf
                 <x-backend.form.input type="text" name="name" label="Full Name" placeholder="Full Name" note='' />
                 <x-backend.form.input type="text" name="username" label="username" placeholder="johndoe23" note='' />
-                
+                <x-backend.form.input type="email" name="email" label="email" placeholder="something@example.com" note='' />
+
                 <div class="d-flex gap-5">
                     <x-backend.form.input type="password" name="password" label="password" placeholder="Password" note='Password must include letters, numbers & symbols' />
                     <x-backend.form.input type="password" name="confirm_password" label="confirm password" placeholder="Confirm Password" note='Password must include letters, numbers & symbols' />
@@ -43,13 +45,13 @@
     </div>
     @enderror
                   </div>
-                
+
                 <div class="d-flex gap-3 align-items-center">
                     <x-backend.form.input type="file" name="profile_image" label="Select Profile Picture" />
                     <div class="mx-3 mt-3">OR</div>
                       <x-backend.form.select-avater />
                 </div>
-                
+
                 <button type="submit" class="btn btn-primary">Register</button>
                 <a href="{{route('admin.login')}}" class="btn ms-2 text-primary text-bold">Login</a>
             </form>
