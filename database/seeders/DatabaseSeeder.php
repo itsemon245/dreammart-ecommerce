69e87d347c731c45cacd3bda861306281002b9b5
Mojahidul Islam <bdemon245@gmail.com>
2023-02-12 17:46:22 +0600

    added category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             EventSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
