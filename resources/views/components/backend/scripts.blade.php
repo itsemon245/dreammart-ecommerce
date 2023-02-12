f87d6a6f0caa239945dd047f0359c9a32cc1cea4
Mojahidul Islam <bdemon245@gmail.com>
2023-02-12 19:00:36 +0600

    fixed links and resources linking proccess in backend dashboard

--- resources/views/components/backend/scripts.blade.php
+++ resources/views/components/backend/scripts.blade.php
@@ -1,21 +1,21 @@
 <!-- Core JS -->
   <!-- build:js assets/vendor/js/core.js -->
-  <script src="../assets/vendor/libs/jquery/jquery.js"></script>
-  <script src="../assets/vendor/libs/popper/popper.js"></script>
-  <script src="../assets/vendor/js/bootstrap.js"></script>
-  <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+  <script src="{{asset('backend/assets/vendor/libs/jquery/jquery.js')}}"></script>
+  <script src="{{asset('backend/assets/vendor/libs/popper/popper.js')}}"></script>
+  <script src="{{asset('backend/assets/vendor/js/bootstrap.js')}}"></script>
+  <script src="{{asset('backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
 
-  <script src="../assets/vendor/js/menu.js"></script>
+  <script src="{{asset('backend/assets/vendor/js/menu.js')}}"></script>
   <!-- endbuild -->
 
   <!-- Vendors JS -->
-  <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+  <script src="{{asset('backend/assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
 
   <!-- Main JS -->
-  <script src="../assets/js/main.js"></script>
+  <script src="{{asset('backend/assets/js/main.js')}}"></script>
 
   <!-- Page JS -->
-  <script src="../assets/js/dashboards-analytics.js"></script>
+  <script src="{{asset('backend/assets/js/dashboards-analytics.js')}}"></script>
 
   <!-- Place this tag in your head or just before your close body tag. -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
