165214272b7157b59df2294fe7719b90932b2ecd
Mojahidul Islam <bdemon245@gmail.com>
2023-02-13 16:18:36 +0600

    added favorites table and built relationship between user & product

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function favorites()
+    {
+        $this->hasMany(Favorite::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
